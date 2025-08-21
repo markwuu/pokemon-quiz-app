294270953714166a1e1da9b40ff136d63af9e189
Mark W <markwuu@gmail.com>
2025-08-20 20:48:00 -0700

    added difficulty buttons to welcome screen

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -12,14 +12,38 @@ const Welcome: FC<IProps> = ({
   startQuiz,
   startButtonDisabled,
 }) => {
+  const difficultyLevels = ["Easy", "Medium", "Hard", "Impossible"];
+
   return (
     // Welcome Screen
-    <div id="welcome-screen" className={`screen ${welcomeScreenActive}`}>
-      <h1 style={{ fontSize: "30px" }}>Test your Pokemon knowledge!</h1>
-      <button onClick={startQuiz} id="start-btn" disabled={startButtonDisabled}>
-        Start Quiz
-      </button>
-    </div>
+    <>
+      <div className={`screen ${welcomeScreenActive}`}>
+        <h1 style={{ fontSize: "30px" }}>Test your Pokemon knowledge!</h1>
+        <button
+          onClick={startQuiz}
+          id="start-btn"
+          disabled={startButtonDisabled}>
+          Start Quiz
+        </button>
+      </div>
+      <div
+        id="difficulty-container"
+        className={`screen ${welcomeScreenActive}`}>
+        {difficultyLevels.map((difficulty, index) => {
+          return (
+            <button
+              onClick={() => {
+                console.log("selecting difficulty");
+              }}
+              key={index}
+              id="difficulty-btn"
+              disabled={startButtonDisabled}>
+              {difficulty}
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
