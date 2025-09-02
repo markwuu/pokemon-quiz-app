2bf114a9317c4e17a6a32d2709e27113dde11184
Mark W <markwuu@gmail.com>
2025-09-02 00:33:40 -0700

    created difficulty enum

--- src/types/Question.ts
+++ src/types/Question.ts
@@ -3,3 +3,10 @@ export interface Question {
   options: string[];
   answer: number;
 }
+
+export enum Difficulty {
+  Easy = "EASY",
+  Mediun = "MEDIUM",
+  Hard = "HARD",
+  Impossible = "IMPOSSIBLE",
+}
