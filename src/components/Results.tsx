04955293ad22b2e761a23c5a293c11a1bf087afc
Mark W <markwuu@gmail.com>
2025-09-07 18:32:54 -0700

    updated colors

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -15,7 +15,7 @@ const Results: FC<IProps> = ({ finalScore, retartQuiz, perfectScore }) => {
         <h1>{`Congratulations! You're a Pokemon Master!`}</h1>
       ) : null}
       <h2>Your Score:</h2>
-      <p id="score-text">{finalScore}</p>
+      <p>{finalScore}</p>
       <button id="restart-btn" onClick={retartQuiz}>
         Restart Quiz
       </button>
