2d781a82804657e2f51f6c90ce641b4fa95a24db
Mark W <markwuu@gmail.com>
2025-09-09 11:00:30 -0700

    updated question type

--- src/types/Question.ts
+++ src/types/Question.ts
@@ -2,6 +2,9 @@ export interface Question {
   question: string;
   options: string[];
   answer: number;
+  cry: string | undefined;
+  image: string | undefined;
+  name: string;
 }
 
 export enum Difficulty {
