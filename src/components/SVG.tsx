2a05516ce54c8a710ae98885d07421c577a00fe4
Mark W <markwuu@gmail.com>
2025-09-22 16:02:02 -0700

    passed in style prop to SVG component

--- src/components/SVG.tsx
+++ src/components/SVG.tsx
@@ -4,10 +4,11 @@ import Icons from "../svg/icons.svg";
 interface IconProps {
   name: string;
   size: number;
+  style: {};
 }
 
-const Icon: FC<IconProps> = ({ name, size }) => (
-  <svg className={`icon icon-${name}`} width={size} height={size}>
+const Icon: FC<IconProps> = ({ name, size, style }) => (
+  <svg className={`icon icon-${name}`} width={size} height={size} style={style}>
     <use xlinkHref={`${Icons}#icon-${name}`} />
   </svg>
 );
