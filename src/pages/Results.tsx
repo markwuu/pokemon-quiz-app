6b1994a5a32922190d3d62a8671e4b71730a7d9e
Mark W <markwuu@gmail.com>
2025-09-24 22:23:09 -0700

    updated result screen text and css

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -33,25 +33,24 @@ const Results: FC<IProps> = ({
             gymBadge === index ? (
               <>
                 <h1>{`Congratulations! You earned the ${badge.charAt(0).toUpperCase()}${badge.replace("-", " ").slice(1)}.`}</h1>
-                <Icon name={badge} size={50} key={index} style={{}} />
+                <Icon name={badge} size={95} key={index} style={{}} />
               </>
             ) : null
           )}
         </div>
       ) : null}
       {perfectScore && difficultyLevel !== Difficulty.Hard ? (
-        <h1>{`Good job! You got all of them right.`}</h1>
+        <h1>{`Wow! Perfect score!`}</h1>
       ) : null}
+      {!perfectScore ? <h1>{`Oh! You almost had it ;)`}</h1> : null}
       <h2>Your Score: [{finalScore}]</h2>
-      <div>
-        {answers?.map(({ answer, correct }: Answer, index: number) => {
-          return (
-            <p key={index}>
-              {index + 1}.{` ${answer} `} {correct ? "✅" : "❌"}
-            </p>
-          );
-        })}
-      </div>
+      {answers?.map(({ answer, correct }: Answer, index: number) => {
+        return (
+          <p key={index}>
+            {index + 1}.{` ${answer} `} {correct ? "✅" : "❌"}
+          </p>
+        );
+      })}
       <button className="next-btn" onClick={restartQuiz}>
         Restart Quiz
       </button>
